962e0199601e8c246bb8b9e9e50a98aea81c725d
chienbui09 <buiducchientlf2209@gmail.com>
2023-05-17 22:32:56 +0700

    first commit

---
+++ src/main/java/com/example/learning_loda/LearningLodaApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_loda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningLodaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningLodaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learning_loda/LearningLodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning_loda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningLodaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
