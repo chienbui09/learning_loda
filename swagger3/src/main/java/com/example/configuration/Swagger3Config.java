8bd3492f2b44f4a9a0d34402840c93add8fd13c4
chienbui09 <buiducchientlf2209@gmail.com>
2023-06-05 00:57:29 +0700

    swagger

--- swagger3/src/main/java/com/example/configuration/Swagger3Config.java
+++ swagger3/src/main/java/com/example/configuration/Swagger3Config.java
@@ -5,14 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.SpringDocUtils;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
-
-import java.util.Arrays;
 
 @Configuration
 public class Swagger3Config {
@@ -21,12 +18,12 @@ public class Swagger3Config {
         return new OpenAPI()
                 .components(new Components())
                 // info
-                .info(new Info().title("Loda Application API")
+                .info(new Info().title("Swagger 3 Application API")
                         .description("Sample OpenAPI 3.0")
                         .contact(new Contact()
-                                .email("loda.namnh@gmail.com")
-                                .name("loda")
-                                .url("https://loda.me/"))
+                                .email("buiducchien241125@gmail.com")
+                                .name("chienbd")
+                                .url("facebook.com/chien241125"))
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://www.apache.org/licenses/LICENSE-2.0.html"))
